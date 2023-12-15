35e402887ba5e8478addf6bb24f5d287d6eb1ae3
xpatxj <patrycjagrzesiak100@gmail.com>
2023-12-15 20:38:58 +0100

    added v

--- lyrics_sentiment.py
+++ lyrics_sentiment.py
@@ -1,26 +1,32 @@
 from textblob import TextBlob
 import lyricsgenius
+from deep_translator import GoogleTranslator
 
 from collections import Counter
 from config import g_access_token
 
 genius = lyricsgenius.Genius('g_access_token')
+
 sentiments = []
 recommendation_final_playlist = {}
-c = []
+a, b, c = [], [], []
 
 # do sentiment analysis on song
 def analyze_sentiment(name, artist):
     try:
         lyrics = genius.search_song(name, artist).lyrics
-        blob = TextBlob(lyrics)
+        is_english = TextBlob(lyrics).detect_language()
+        if is_english != 'en':
+            t_lyrics = GoogleTranslator(source='auto', target='english').translate(lyrics)
+            blob = TextBlob(t_lyrics)
+        else:
+            blob = TextBlob(lyrics)
         return blob.sentiment.polarity
     except AttributeError:
         pass
     except UnboundLocalError:
         pass
     
-
 # get playlist and do sentiment analysis on each song
 def get_playlist(list_of_songs):
     for name, artist in list_of_songs.items():
@@ -28,12 +34,12 @@ def get_playlist(list_of_songs):
     return sentiments
 
 
-
+# give recommendations based on sentiment analysis
 def get_playlist_recommendations(list_of_songs):
     
     for name, artist in list_of_songs.items():
         s = round(analyze_sentiment(name, artist), 1)
-        if s is not None and (s == 0.2 or s == 0.1 or s == 0.3):
+        if s is not None and (s == a[0] or s == b[0] or s == c[0]):
             recommendation_final_playlist[name] = artist
             print(s)
         elif len(recommendation_final_playlist) >= 10:
@@ -45,5 +51,5 @@ def get_playlist_recommendations(list_of_songs):
 
 #get most popular three songs from playlist
 def get_most_popular():
-    c = Counter(sentiments).most_common(3)
-    return c
+    a,b,c = Counter(sentiments).most_common(3)
+    return a,b,c
--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from spotipy.oauth2 import SpotifyOAuth
 from config import s_client_id, s_client_secret, redirect_uri, playlist_url
 from lyrics_sentiment import get_playlist, get_most_popular, get_playlist_recommendations, recommendation_final_playlist
 
-
 scope = 'playlist-read-private playlist-modify-public'
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=s_client_id, client_secret=s_client_secret, redirect_uri=redirect_uri, scope=scope))
 
 user_info = sp.current_user()
 user_id = user_info['id']
+
+
 
 # playlist_url = input("Please enter URL of playlist: ")
 
@@ -26,14 +27,12 @@ for track_id in track_ids:
     tracks_info[track['name']] = artists
 
 print(tracks_info)
-sentiment = get_playlist(tracks_info)
-most_common = get_most_popular()
-print("The most popular sentiment in this playlist is: " + str(most_common))
-print(sentiment)
+get_playlist(tracks_info)
+get_most_popular()
+print(get_most_popular())
 
 seed_tracks = track_ids[:5]
-
-recommendations = sp.recommendations(seed_tracks=seed_tracks, limit=10)
+recommendations = sp.recommendations(seed_tracks=seed_tracks, limit=30)
 
 recommendations_ids = [track['id'] for track in recommendations['tracks']]
 recommendations_info = {}
@@ -45,6 +44,7 @@ for r_id in recommendations_ids:
 get_playlist_recommendations(recommendations_info)
 print(recommendations_info)
 print(recommendation_final_playlist)
+
 new_playlist = sp.user_playlist_create(user=user_id, name='recommended<3', public=True, description='recommended songs based on playlist')
 
 for name, artist in recommendation_final_playlist.items():
