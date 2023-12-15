3c59b42c1646c3beb9c8269f95ffbf11167327f6
xpatxj <patrycjagrzesiak100@gmail.com>
2023-12-15 19:44:02 +0100

    added fixed playlist_recommendations

--- lyrics_sentiment.py
+++ lyrics_sentiment.py
@@ -6,22 +6,42 @@ from config import g_access_token
 
 genius = lyricsgenius.Genius('g_access_token')
 sentiments = []
+recommendation_final_playlist = {}
+c = []
 
 # do sentiment analysis on song
 def analyze_sentiment(name, artist):
     try:
         lyrics = genius.search_song(name, artist).lyrics
         blob = TextBlob(lyrics)
+        return blob.sentiment.polarity
     except AttributeError:
         pass
-    return blob.sentiment.polarity
+    except UnboundLocalError:
+        pass
+    
 
 # get playlist and do sentiment analysis on each song
 def get_playlist(list_of_songs):
     for name, artist in list_of_songs.items():
-        # sentiments.append(round(analyze_sentiment(name, artist), 1))
-        sentiments.append(analyze_sentiment(name, artist))
+        sentiments.append(round(analyze_sentiment(name, artist), 1))
     return sentiments
+
+
+
+def get_playlist_recommendations(list_of_songs):
+    
+    for name, artist in list_of_songs.items():
+        s = round(analyze_sentiment(name, artist), 1)
+        if s is not None and (s == 0.2 or s == 0.1 or s == 0.3):
+            recommendation_final_playlist[name] = artist
+            print(s)
+        elif len(recommendation_final_playlist) >= 10:
+            break
+        else:
+            pass
+        
+    return recommendation_final_playlist
 
 #get most popular three songs from playlist
 def get_most_popular():
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from config import s_client_id, s_client_secret, redirect_uri, playlist_url
-from lyrics_sentiment import get_playlist, get_most_popular
+from lyrics_sentiment import get_playlist, get_most_popular, get_playlist_recommendations, recommendation_final_playlist
 
 
 scope = 'playlist-read-private playlist-modify-public'
@@ -20,22 +20,38 @@ tracks = sp.playlist_tracks(playlist_id)
 tracks_info = {}
 track_ids = [track['track']['id'] for track in tracks['items']]
 
-# for track_id in track_ids:
-#     track = sp.track(track_id)
-#     artists = ', '.join([artist['name'] for artist in track['artists']])
-#     tracks_info[track['name']] = artists
+for track_id in track_ids:
+    track = sp.track(track_id)
+    artists = ', '.join([artist['name'] for artist in track['artists']])
+    tracks_info[track['name']] = artists
 
-# sentiment = get_playlist(tracks_info)
-# most_common = get_most_popular()
-# print("The most popular sentiment in this playlist is: " + str(most_common))
-# print(sentiment)
-
-# new_playlist = sp.user_playlist_create(user=user_id, name='recommended<3', public=True, description='recommended songs based on playlist')
+print(tracks_info)
+sentiment = get_playlist(tracks_info)
+most_common = get_most_popular()
+print("The most popular sentiment in this playlist is: " + str(most_common))
+print(sentiment)
 
 seed_tracks = track_ids[:5]
 
-recommendations = sp.recommendations(seed_tracks=seed_tracks, limit=20)
-print(recommendations)
+recommendations = sp.recommendations(seed_tracks=seed_tracks, limit=10)
 
-# track_uris = [track['uri'] for track in recommendations['tracks']]
-# sp.playlist_add_items(new_playlist['id'], track_uris)
+recommendations_ids = [track['id'] for track in recommendations['tracks']]
+recommendations_info = {}
+for r_id in recommendations_ids:
+    track = sp.track(r_id)
+    artists = ', '.join([artist['name'] for artist in track['artists']])
+    recommendations_info[track['name']] = artists
+
+get_playlist_recommendations(recommendations_info)
+print(recommendations_info)
+print(recommendation_final_playlist)
+new_playlist = sp.user_playlist_create(user=user_id, name='recommended<3', public=True, description='recommended songs based on playlist')
+
+for name, artist in recommendation_final_playlist.items():
+    search = sp.search(q='track:' + name + ' artist:' + artist, type='track')
+    try:
+        track_id = search['tracks']['items'][0]['id']
+        sp.playlist_add_items(playlist_id=new_playlist['id'], items=[track_id])
+        print(f"track {name} by {artist} added to playlist")
+    except IndexError:
+        print("Index error")
