7a1c966045e1c888d11a6119afb61b0208b098f1
xpatxj <patrycjagrzesiak100@gmail.com>
2023-12-24 01:24:25 +0100

    added option 2

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from config import s_client_id, s_client_secret, redirect_uri, playlist_url
 from lyrics_sentiment import get_playlist, get_most_popular, get_playlist_recommendations, recommendation_final_playlist
+import matplotlib.pyplot as plt
 
 scope = 'playlist-read-private playlist-modify-public'
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=s_client_id, client_secret=s_client_secret, redirect_uri=redirect_uri, scope=scope))
@@ -9,49 +10,121 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=s_client_id, client_sec
 user_info = sp.current_user()
 user_id = user_info['id']
 
-
-
 # playlist_url = input("Please enter URL of playlist: ")
+chose = input("""Choose an option: 1. 10 new tracks recommendation based on playlist's songs' sentiment analysis.
+      2. Chart based on your playlist: BPM, velance, energy.
+      3. Choose your zodiac sign and we'll made a playlist for you based on your favorite playlist! 
+        Warning: it's highly stereotipical and might be not accurate at all.""")
 
-playlist_url = playlist_url.split('?')[0]
-playlist_id = playlist_url.split('/')[-1]
 
-tracks = sp.playlist_tracks(playlist_id)
-tracks_info = {}
-track_ids = [track['track']['id'] for track in tracks['items']]
 
-for track_id in track_ids:
-    track = sp.track(track_id)
-    artists = ', '.join([artist['name'] for artist in track['artists']])
-    tracks_info[track['name']] = artists
 
-print(tracks_info)
-get_playlist(tracks_info)
-get_most_popular()
-print(get_most_popular())
+def get_ids():
 
-seed_tracks = track_ids[:5]
-recommendations = sp.recommendations(seed_tracks=seed_tracks, limit=30)
+    playlist_url = playlist_url.split('?')[0]
+    playlist_id = playlist_url.split('/')[-1]
 
-recommendations_ids = [track['id'] for track in recommendations['tracks']]
-recommendations_info = {}
-for r_id in recommendations_ids:
-    track = sp.track(r_id)
-    artists = ', '.join([artist['name'] for artist in track['artists']])
-    recommendations_info[track['name']] = artists
+    tracks = sp.playlist_tracks(playlist_id)
+    
+    track_ids = [track['track']['id'] for track in tracks['items']]
 
-get_playlist_recommendations(recommendations_info, sp)
-print(recommendations_info)
-print(recommendation_final_playlist)
+    return playlist_id, track_ids
 
-name = input("Please enter name of playlist: ")
-new_playlist = sp.user_playlist_create(user=user_id, name=name, public=True, description='recommended songs based on playlist')
+def option_one(track_ids):
 
-for name, artist in recommendation_final_playlist.items():
-    search = sp.search(q='track:' + name + ' artist:' + artist, type='track')
-    try:
-        track_id = search['tracks']['items'][0]['id']
-        sp.playlist_add_items(playlist_id=new_playlist['id'], items=[track_id])
-        print(f"track {name} by {artist} added to playlist")
-    except IndexError:
-        print("Index error")
+    tracks_info = {}
+    for track_id in track_ids:
+        track = sp.track(track_id)
+        artists = ', '.join([artist['name'] for artist in track['artists']])
+        tracks_info[track['name']] = artists
+
+    print(tracks_info)
+    get_playlist(tracks_info)
+    get_most_popular()
+    print(get_most_popular())
+
+    seed_tracks = track_ids[:5]
+    recommendations = sp.recommendations(seed_tracks=seed_tracks, limit=30)
+
+    recommendations_ids = [track['id'] for track in recommendations['tracks']]
+    recommendations_info = {}
+    for r_id in recommendations_ids:
+        track = sp.track(r_id)
+        artists = ', '.join([artist['name'] for artist in track['artists']])
+        recommendations_info[track['name']] = artists
+
+    get_playlist_recommendations(recommendations_info, sp)
+    print(recommendations_info)
+    print(recommendation_final_playlist)
+
+    name = input("Please enter name of playlist: ")
+    new_playlist = sp.user_playlist_create(user=user_id, name=name, public=True, description='recommended songs based on playlist')
+
+    for name, artist in recommendation_final_playlist.items():
+        search = sp.search(q='track:' + name + ' artist:' + artist, type='track')
+        try:
+            track_id = search['tracks']['items'][0]['id']
+            sp.playlist_add_items(playlist_id=new_playlist['id'], items=[track_id])
+            print(f"track {name} by {artist} added to playlist")
+        except IndexError:
+            print("Index error")
+
+def option_two(playlist_id, track_ids, option):
+    
+    features = []
+    name_of_feature = ''
+    if option == 'bpm':
+        for track_id in track_ids:
+            audio_features = sp.audio_features([track_id])[0]
+            features.append(audio_features['tempo'])
+            name_of_feature = 'BPM'
+    
+    if option == 'valence':
+        for track_id in track_ids:
+            audio_features = sp.audio_features([track_id])[0]
+            features.append(audio_features['valence'])
+            name_of_feature = 'Valence'
+
+    if option == 'energy':
+        for track_id in track_ids:
+            audio_features = sp.audio_features([track_id])[0]
+            features.append(audio_features['energy'])
+            name_of_feature = 'Energy'
+    
+    color = get_dominant_color(user_info['images'][0]['url'])
+
+    plt.hist(features, bins=range(min(features), max(features) + 4, 4), color=color, edgecolor='black')
+
+    plt.xlabel(name_of_feature)
+    plt.ylabel('count')
+
+    plt.show()
+
+def option_three():
+    print("option three")
+
+def get_dominant_color(url):
+
+    from PIL import Image
+    import requests
+    from io import BytesIO
+
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+
+    img = img.resize((1, 1))
+
+    dominant_color = img.getpixel((0, 0))
+
+    return dominant_color
+
+if chose == '1':
+    playlist_id, track_ids = get_ids()
+    option_one(track_ids)
+elif chose == '2':
+    playlist_id, track_ids = get_ids()
+    option = input("Choose an option: 1. BPM 2. Valence 3. Energy. \nType the number of option:")
+    option_two(playlist_id, track_ids, option)
+
+elif chose == '3':
+    option_three()
